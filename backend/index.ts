c695a92559258aab0f0c25b26f6b464ea2d5772a
Yit-hub <danielyi2004@gmail.com>
2025-06-03 14:13:40 +0200

    Productos.ts

--- backend/index.ts
+++ backend/index.ts
@@ -2,10 +2,12 @@ import express from 'express'
 import cors from 'cors'
 import empleadosRoutes from './routes/empleados'
 import encargosRoutes from './routes/encargos'
+import productosRoutes from './routes/productos'
+
 
 const app = express()
 
-// Permitir solicitudes desde tu frontend (localhost:5173)
+// Permitir solicitudes desde el frontend (localhost:5173)
 app.use(cors({
   origin: 'http://localhost:5173',
   credentials: true
@@ -15,6 +17,8 @@ app.use(express.json())
 
 app.use('/api/empleados', empleadosRoutes)
 app.use('/api/encargos', encargosRoutes)
+app.use('/api/productos', productosRoutes)
+
 
 app.listen(3001, () => {
   console.log('Servidor backend corriendo en http://localhost:3001')
